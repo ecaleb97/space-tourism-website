0d49362404f2f756c67dcdbaa181d58a6ca5581c
ecaleb97 <valley_rob.0c@icloud.com>
2024-02-26 15:11:30 +0100

    Add paths and compiler options to tsconfig.json, update dependencies in package.json, add data and tailwind configuration files

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'main-desktop': "url('../src/assets/home/background-home-desktop.jpg')",
+				'main-mobile': "url('../src/assets/home/background-home-mobile.jpg')",
+				'main-tablet': "url('../src/assets/home/background-home-tablet.jpg')",
+				'destination-desktop': "url('../src/assets/destination/background-destination-desktop.jpg')",
+				'destination-mobile': "url('../src/assets/destination/background-destination-mobile.jpg')",
+				'destination-tablet': "url('../src/assets/destination/background-destination-tablet.jpg')",
+			},
+			colors: {
+				primary: '#D0D6F9'
+			}
+		},
+	},
+	plugins: [],
+}
