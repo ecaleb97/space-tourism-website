94688d2be1314df0e1b3a3b3662ed08997ad52ab
ecaleb97 <valley_rob.0c@icloud.com>
2024-02-26 15:36:55 +0100

    Update file paths for background images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,12 +4,12 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'main-desktop': "url('./src/assets/home/background-home-desktop.jpg')",
-				'main-mobile': "url('./src/assets/home/background-home-mobile.jpg')",
-				'main-tablet': "url('./src/assets/home/background-home-tablet.jpg')",
-				'destination-desktop': "url('./src/assets/destination/background-destination-desktop.jpg')",
-				'destination-mobile': "url('./src/assets/destination/background-destination-mobile.jpg')",
-				'destination-tablet': "url('./src/assets/destination/background-destination-tablet.jpg')",
+				'main-desktop': "url('/src/assets/home/background-home-desktop.jpg')",
+				'main-mobile': "url('/src/assets/home/background-home-mobile.jpg')",
+				'main-tablet': "url('/src/assets/home/background-home-tablet.jpg')",
+				'destination-desktop': "url('/src/assets/destination/background-destination-desktop.jpg')",
+				'destination-mobile': "url('/src/assets/destination/background-destination-mobile.jpg')",
+				'destination-tablet': "url('/src/assets/destination/background-destination-tablet.jpg')",
 			},
 			colors: {
 				primary: '#D0D6F9'
